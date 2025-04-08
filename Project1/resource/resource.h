8d591cf6cd0c35c0956c436b452f0266afacea63
unkonow <lllfamily132@yahoo.co.jp>
2025-04-08 23:31:08 +0900

    CMakeで動くように

---
+++ Project1/resource/resource.h
@@ -0,0 +1,16 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+// リソースID定義
+#define IDD_CONFIRM_DIALOG 101
+#define IDI_ICON1          102
+
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDC_TREEVIEW                      1001
+#define IDC_OK_BUTTON                     1002
+#define IDC_CANCEL_BUTTON                 1003
+
+#endif // RESOURCE_H
